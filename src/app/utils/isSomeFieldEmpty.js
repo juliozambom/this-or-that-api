df8d647a8543f15706678a6940f9d82dfed326b4
Julio Zambom <work.juliozambom@gmail.com>
2022-11-11 14:54:11 -0300

    fix: fixing isSomeFieldEmpty util

--- src/app/utils/isSomeFieldEmpty.js
+++ src/app/utils/isSomeFieldEmpty.js
@@ -1,5 +1,10 @@
 function isSomeFieldEmpty(fields) {
-  return fields.includes(undefined && "");
+  const isUndefined = fields.includes(undefined);
+  const isEmptyString = fields.includes("");
+
+  if (isUndefined || isEmptyString) {
+    return true;
+  }
 }
 
 module.exports = isSomeFieldEmpty;
