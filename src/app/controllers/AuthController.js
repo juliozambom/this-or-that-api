7b977e730f260f8ea19510e796252423f9d239ee
Julio Zambom <work.juliozambom@gmail.com>
2022-11-11 21:26:09 -0300

    feat: creating admin model and admin auth

--- src/app/controllers/AuthController.js
+++ src/app/controllers/AuthController.js
@@ -1,3 +1,6 @@
+const { PrismaClient } = require("@prisma/client");
+const { admin } = new PrismaClient();
+
 const UsersRepository = require("../repositories/UsersRepository");
 const isSomeFieldEmpty = require("../utils/isSomeFieldEmpty");
 const bcrypt = require("bcrypt");
@@ -38,6 +41,46 @@ class AuthController {
 
     return res.status(200).json({
       message: "Login realizado com sucesso",
+      token,
+    });
+  }
+
+  async adminLogin(req, res) {
+    const { email, password } = req.body;
+
+    const emptyFieldExists = isSomeFieldEmpty([email, password]);
+
+    if (emptyFieldExists) {
+      return res.status(400).json({
+        message: "Campos obrigatórios não foram enviados",
+        token: null,
+      });
+    }
+
+    const adminExists = await admin.findFirst({
+      where: {
+        email,
+      },
+    });
+
+    if (!adminExists) {
+      return res.status(401).json({
+        message: "Não conseguimos encontrar um admin com o email especificado",
+        token: null,
+      });
+    }
+
+    if (password !== adminExists?.password) {
+      return res.status(401).json({
+        message: "Senha incorreta",
+        token: null,
+      });
+    }
+
+    const token = createToken(adminExists?.id);
+
+    return res.status(200).json({
+      message: "Admin logado com sucesso",
       token,
     });
   }
--- src/app/routes/AuthRoutes.js
+++ src/app/routes/AuthRoutes.js
@@ -4,5 +4,6 @@ const router = Router();
 const AuthController = require("../controllers/AuthController");
 
 router.post("/auth/login", AuthController.login);
+router.post("/auth/admin-login", AuthController.adminLogin);
 
 module.exports = router;
