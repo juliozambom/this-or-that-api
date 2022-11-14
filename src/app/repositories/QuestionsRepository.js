558ff90e74d9187448f1d9eb3673645c8527a7bb
Julio Zambom <work.juliozambom@gmail.com>
2022-11-14 13:40:30 -0300

    feat: returning users information in findQuestions method

--- src/app/repositories/QuestionsRepository.js
+++ src/app/repositories/QuestionsRepository.js
@@ -78,6 +78,9 @@ class QuestionsRepository {
         where: {
           is_validated: true,
         },
+        include: {
+          user: true,
+        },
       });
 
       return validatedQuestions;
